28cfffd36e2f7f40469c0d2fc94bea0875a14d88
Dima <dima.aidinov@gmail.com>
2023-03-03 19:24:40 +0200

    Second Commit

---
+++ HelloWorld/Class1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+namespace HelloWorld
+{
+    public class Evaluate 
+    {
+        public static double EvaluateExpression(string expression)
+        {
+            // Удаляем пробелы в начале и в конце строки
+            expression = expression.Trim();
+
+            // Инициализируем объект-вычислитель математических выражений
+            System.Data.DataTable table = new System.Data.DataTable();
+
+            // Вычисляем значение выражения и возвращаем его в виде числа
+            return Convert.ToDouble(table.Compute(expression, ""));
+        }
+
+    }
+}
+   
+
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,26 @@
 ﻿
-Console.WriteLine("Hello, World!");
+using HelloWorld;
+
+public class ReadFromFile : Evaluate
+{
+    static void Main(string[] args)
+    {
+
+
+        // Считываем содержимое файла
+        string text = System.IO.File.ReadAllText(@"D:\readText\WriteText.txt");
+
+        
+        // Разбиваем содержимое файла на массив строк
+        string[] lines = text.Split('\n');
+
+        
+        // Проходим по всем строкам файла
+        foreach (string line in lines)
+        {
+            // Выполняем математическую операцию над примером и выводим результат в консоль
+            Console.WriteLine(line + " = " + EvaluateExpression(line));
+        }
+
+    }
+}
